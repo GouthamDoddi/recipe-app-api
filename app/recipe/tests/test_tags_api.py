cfa1f6ca01865b8f26339aa2fbd57c8acc60f53b
GouthamDoddi <gouthambolt@gmail.com>
2020-05-15 16:04:48 +0530

    added Tag serializer along with passed tests

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -5,7 +5,8 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from recipe.models import Tag
+
+from core.models import Tag
 
 from recipe.serializers import TagSerializer
 
@@ -33,7 +34,7 @@ class PrivateTagsApiTests(TestCase):
            email='testman@mansmail.com',
            password='test123',
         )
-        self.client = APIClient
+        self.client = APIClient()
 
         self.client.force_authenticate(self.user)
 
@@ -48,18 +49,18 @@ class PrivateTagsApiTests(TestCase):
         serializer = TagSerializer(tags, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
-        
+
     def test_tags_limited_to_user(self):
         """Test that tags returned are for the authorized user"""
         user2 = get_user_model().objects.create_user(
-            email='testman@mansmail.com',
+            email='testman2@mansmail.com',
             password='test123',
         )
         Tag.objects.create(user=user2, name='fruity')
         tag = Tag.objects.create(user=self.user, name='Comfort Food')
 
         res = self.client.get(TAGS_URL)
-        
+
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
-        self.assertEqual(res.data[0], tag.name)
+        self.assertEqual(res.data[0]['name'], tag.name)
